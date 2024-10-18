bdc082085f26c7e7b9b74bb8a8b7f4ae018ff6e9
Илья <@DESKTOP-VPI29MR>
2024-10-18 09:52:33 +0200

    Добавьте файлы проекта.

---
+++ lab12/Program.cs
@@ -0,0 +1,31 @@
+﻿try
+{
+    Console.Write("Введите значение k : ");
+    int k = int.Parse(Console.ReadLine());
+    Console.Write("Введите значение l : ");
+    int l = int.Parse(Console.ReadLine());
+    Console.Write("Введите значение n : ");
+    int n = int.Parse(Console.ReadLine());
+    Console.Write("Введите значение m : ");
+    int m = int.Parse(Console.ReadLine());
+
+    //int k = 0; 
+    //int l = 5; 
+    //int n = 3; 
+    //int m = 4; 
+
+    bool condition = (k == 0 && l > m) || (k < 0 && 2 * l - 3 * n < m);
+
+    if (condition)
+    {
+        Console.WriteLine("Условие истинно");
+    }
+    else
+    {
+        Console.WriteLine("Условие ложно");
+    }
+}
+catch 
+{
+    Console.WriteLine("Error");
+}
